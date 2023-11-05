ce643d31e8578d7658fb9f48916620da957ba97f
Babayomi Oluwasuyi <61556760+yomzeew@users.noreply.github.com>
2023-11-05 10:48:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const corsOptions = {
-  origin: ["https://robofrontend.vercel.app", "http://localhost:3001"],
+  origin: ["http://robofrontend.vercel.app", "http://localhost:3001"],
 };
 var app = express();
 
